7c94ffdf4336a33d9670053304080438ca2b31c5
luis torres <luis.torres@blueconic.com>
2026-01-11 19:38:45 -0500

    feat(frontend): add sort buttons name and number

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,37 +1,20 @@
-import { Search } from "lucide-react";
+// components/Navbar.tsx
 import Image from "next/image";
-import { Input } from "@/components/ui/input";
+import SearchFilters from "./SearchFilters";
 
 export default function Navbar() {
   return (
-    <header className="bg-[#DC0A2D] pt-12 pb-6 px-6">
-      <div className="max-w-7xl mx-auto flex flex-col gap-4">
-        <div className="flex items-center gap-3">
-          <Image
-            src="/pokeball.svg.png"
-            alt="Pokeball"
-            width={32}
-            height={32}
-          />
+    <header className="bg-[#DC0A2D] w-full pt-12 pb-6 px-6">
+      <div className="max-w-7xl mx-auto flex flex-col gap-5">
+        <div className="flex items-center gap-4">
+          <Image src="/pokeball.svg.png" alt="Pokeball" width={32} height={32} />
           <h1 className="text-white text-3xl font-bold tracking-tight">
             Pokédex
           </h1>
         </div>
 
-        <div className="relative w-full max-w-2xl group">
-          <Search
-            className="absolute left-4 top-1/2 -translate-y-1/2 text-[#DC0A2D] z-10"
-            size={18}
-          />
-
-          <Input
-            type="text"
-            placeholder="Search"
-            className="w-full h-11 pl-12 pr-4 rounded-full bg-white border-none shadow-inner
-                     focus-visible:ring-2 focus-visible:ring-white/50
-                     placeholder:text-gray-400 font-light text-gray-700"
-          />
-        </div>
+        {/* Invocamos el nuevo componente */}
+        <SearchFilters />
       </div>
     </header>
   );
